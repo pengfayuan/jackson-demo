18f78d182fc685fea858399bf82ab4a6eaaaa0d4
fayuan.peng <fa@mycredigo.com>
2021-08-21 18:25:59 +0800

    提交代码

---
+++ src/main/java/com/yuanyuan/jacksondemo/JacksonDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yuanyuan.jacksondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacksonDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuanyuan/jacksondemo/JacksonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanyuan.jacksondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
